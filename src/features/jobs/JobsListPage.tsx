842bba581b6a1051ee79635e9b3ad2a9d094d640
Adnan Anjum <anjumcode12@gmail.com>
2026-07-12 13:19:31 +0500

    listed jobs on jobslistpage

--- src/features/jobs/JobsListPage.tsx
+++ src/features/jobs/JobsListPage.tsx
@@ -1,10 +1,13 @@
 import JobCard from "./JobCard";
 import { mockJobs } from "./mockJobs";
+
 const JobsListPage = () => {
   return (
-    <h1>
-      <JobCard job={mockJobs[0]} />
-    </h1>
+    <div className="max-w-2xl mx-auto flex flex-col gap-4 py-8 px-4">
+      {mockJobs.map((job) => (
+        <JobCard key={job._id} job={job} />
+      ))}
+    </div>
   );
 };
 
