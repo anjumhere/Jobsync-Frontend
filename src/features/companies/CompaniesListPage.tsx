3cd5b1d87289fe183c5614821f4125a15a7ab860
Adnan Anjum <anjumcode12@gmail.com>
2026-07-12 13:46:26 +0500

    add mockcompanies data and rendered company card

--- src/features/companies/CompaniesListPage.tsx
+++ src/features/companies/CompaniesListPage.tsx
@@ -1,5 +1,14 @@
+import CompanyCard from "./CompanyCard";
+import { mockCompanies } from "./mockCompanies";
+
 const CompaniesListPage = () => {
-  return <div>Company List</div>;
+  return (
+    <div className="max-w-2xl mx-auto py-8 px-4">
+      {mockCompanies.map((company) => (
+        <CompanyCard key={company._id} company={company} />
+      ))}
+    </div>
+  );
 };
 
 export default CompaniesListPage;
